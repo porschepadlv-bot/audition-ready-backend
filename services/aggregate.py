3cec81058bc0b557c04556300eaeb9922449d2b7
Kenny Austin <porschepadlv@gmail.com>
2026-04-23 15:16:34 -0700

    clean aggregate final

--- services/aggregate.py
+++ services/aggregate.py
@@ -1,7 +1,9 @@
+from sources.craigslist import search_craigslist
 from sources.indeed import search_indeed
 from sources.backstage import search_backstage
 from typing import List
 from models import Listing
+
 
 def aggregate_results(query: str) -> List[Listing]:
     results: List[Listing] = []
@@ -15,5 +17,10 @@ def aggregate_results(query: str) -> List[Listing]:
         results.extend(search_indeed(query))
     except Exception as e:
         print("Indeed error:", e)
+
+    try:
+        results.extend(search_craigslist(query))
+    except Exception as e:
+        print("Craigslist error:", e)
 
     return results[:10]
